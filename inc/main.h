d43bff51f184f0a041382a23e9d35071f28dddf9
lostbtw <bozo@>
2025-08-01 09:41:12 +0200

    added a basic running loop with exit option

--- inc/main.h
+++ inc/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define MAX_COMMAND_SIZE 1024
 #include <stdio.h>
+#include <stdbool.h>
+#include <malloc.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 int main() {
-    printf("Hello, World!\n");
+    bool running = true;
+    char* command = (char*) calloc(MAX_COMMAND_SIZE, sizeof(char));
+
+    while(running){
+        printf("Calculator\n");
+        printf("Write your expression (e.g. '24+656=')\n");
+        printf("To exit the program write 'e' or 'exit' (case insensitive)\n");
+        scanf("%s", command);
+        if(*command == 'e'){
+            running = false;
+        }
+    }
+
+    free(command);
     return 0;
 }
